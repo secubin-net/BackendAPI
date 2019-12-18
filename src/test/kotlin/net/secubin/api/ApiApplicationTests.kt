b3c52a327e0b047f825295fd7e3c1c23ffd007c4
Jonas Hielscher <jonas.hielscher@hotmail.de>
2019-12-18 10:34:58 +0100

    chore: added a small comment to trigger the travis build.

--- src/test/kotlin/net/secubin/api/ApiApplicationTests.kt
+++ src/test/kotlin/net/secubin/api/ApiApplicationTests.kt
@@ -3,6 +3,9 @@ package net.secubin.api
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
+/**
+ * Tests that the Spring DI-container starts.
+ */
 @SpringBootTest
 class ApiApplicationTests {
 
