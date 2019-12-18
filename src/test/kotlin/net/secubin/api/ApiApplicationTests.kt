139130e24afc778a11e89b00d1633c4a7a3b9222
Jonas Hielscher <jonas.hielscher@hotmail.de>
2019-12-18 10:24:25 +0100

    feat: added a base spring-kotlin-13-structure and added a bunch of lines to the gitignore.

---
+++ src/test/kotlin/net/secubin/api/ApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package net.secubin.api
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
